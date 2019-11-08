d432afbe6f1016a0acac890e8448ed4ebeb21ef1
CregskiN <272473132@qq.com>
2019-11-08 22:21:51 +0800

    :pushpin: init demo

---
+++ demo/_fs.js
@@ -0,0 +1,6 @@
+//引用 fs 模块
+const fs = require('fs');
+
+fs.readFile('./importModule.js',(err,data) => {
+    
+})
---
+++ demo/importModule.js
@@ -0,0 +1,5 @@
+const mod = require('./index');
+
+console.log(mod.testVar);
+
+mod.testFn();
